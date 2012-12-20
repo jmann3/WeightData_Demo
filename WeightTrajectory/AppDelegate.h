40982635ab252d4b9d9e00f18fa56ecccbee5c17
jmann3 <jrmann120@gmail.com>
2012-12-20 09:46:31 -0500

    Initial Commit

---
+++ WeightTrajectory/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  WeightTrajectory
+//
+//  Created by John Mann on 12/20/12.
+//  Copyright (c) 2012 John Mann. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ WeightTrajectory/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  WeightTrajectory
+//
+//  Created by John Mann on 12/20/12.
+//  Copyright (c) 2012 John Mann. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
